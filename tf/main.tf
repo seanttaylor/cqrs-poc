c7f5c54ac340106f2e2d7e2ce0bf8d308fc5fbf0
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-06-02 20:09:59 +0000

    CHANGED PROFILE IN AWS PROVIDER TF CONFIG

--- tf/main.tf
+++ tf/main.tf
@@ -28,7 +28,7 @@ terraform {
 provider "aws" {
   access_key                  = var.access_key
   secret_key                  = var.secret_key 
-  profile                     = "default"
+  #profile                     = "default"
   region                      = "us-east-1"
 }
 
