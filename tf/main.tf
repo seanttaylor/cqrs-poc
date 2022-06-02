f40a06ae7c31ab1711beed7641b420e96ce58bf9
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-06-02 19:46:18 +0000

    UPDATED DEPRECATED BUCKET ACL CONFIG

--- tf/main.tf
+++ tf/main.tf
@@ -43,16 +43,21 @@ variable "TFC_CONFIGURATION_VERSION_GIT_COMMIT_SHA" {
 }
 
 resource "random_pet" "lambda_bucket_name" {
-  prefix = "learn-terraform-functions"
+  prefix = "hello-world"
   length = 4
 }
 
 resource "aws_s3_bucket" "lambda_bucket" {
   bucket = random_pet.lambda_bucket_name.id
 
-  acl           = "private"
   force_destroy = true
 }
+
+resource "aws_s3_bucket_acl" "lambda_bucket" {
+  bucket = aws_s3_bucket.lambda_bucket.id
+  acl    = "private"
+}
+
 data "archive_file" "lambda_hello_world" {
   type = "zip"
 
