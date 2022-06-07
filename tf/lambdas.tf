20df043dd2ce4c82115b827f73a38f5932c9cf23
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-06-07 22:38:14 +0000

    ADDED TF CONFIGURATION FOR 'route...' FUNCTION

--- tf/lambdas.tf
+++ tf/lambdas.tf
@@ -85,3 +85,25 @@ resource "aws_cloudwatch_log_group" "enrich_incoming_msg" {
 
   retention_in_days = 30
 }
+
+################## route_incoming_msg LAMBDA CONFIGURATION ###################
+
+resource "aws_lambda_function" "route_incoming_msg" {
+  function_name = "enrich-incoming-msg-${local.git_commit_sha}"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_route_incoming_msg.key
+
+  runtime = "nodejs16.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_route_incoming_msg.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "route_incoming_msg" {
+  name = "/aws/lambda/${aws_lambda_function.route_incoming_msg.function_name}"
+
+  retention_in_days = 30
+}
