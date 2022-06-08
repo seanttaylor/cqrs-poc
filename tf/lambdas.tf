681314d13dc81abdb98df38dc1a81c2997fe48bb
Sean Travis Taylor <sean.travis.taylor@gmail.com>
2022-06-08 00:26:02 +0000

    [WIP]

--- tf/lambdas.tf
+++ tf/lambdas.tf
@@ -42,27 +42,6 @@ resource "aws_iam_role_policy_attachment" "lambda_policy" {
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
 
-################## validate_incoming_msg_header LAMBDA CONFIGURATION ###################
-
-resource "aws_lambda_function" "validate_incoming_msg_header" {
-  function_name = "validate-incoming-msg-header-${local.git_commit_sha}"
-
-  s3_bucket = aws_s3_bucket.lambda_bucket.id
-  s3_key    = aws_s3_object.lambda_validate_incoming_msg_header.key
-
-  runtime = "nodejs16.x"
-  handler = "index.handler"
-
-  source_code_hash = data.archive_file.lambda_validate_incoming_msg_header.output_base64sha256
-
-  role = aws_iam_role.lambda_exec.arn
-}
-
-resource "aws_cloudwatch_log_group" "validate_incoming_msg_header" {
-  name = "/aws/lambda/${aws_lambda_function.validate_incoming_msg_header.function_name}"
-
-  retention_in_days = 30
-}
 
 ################## enrich_incoming_msg LAMBDA CONFIGURATION ###################
 
--- tf/validate-incoming-msg-header.tf
+++ tf/validate-incoming-msg-header.tf
@@ -13,3 +13,23 @@ resource "aws_s3_object" "lambda_validate_incoming_msg_header" {
 
   etag = filemd5(data.archive_file.lambda_validate_incoming_msg_header.output_path)
 }
+
+resource "aws_lambda_function" "validate_incoming_msg_header" {
+  function_name = "validate-incoming-msg-header-${local.git_commit_sha}"
+
+  s3_bucket = aws_s3_bucket.lambda_bucket.id
+  s3_key    = aws_s3_object.lambda_validate_incoming_msg_header.key
+
+  runtime = "nodejs16.x"
+  handler = "index.handler"
+
+  source_code_hash = data.archive_file.lambda_validate_incoming_msg_header.output_base64sha256
+
+  role = aws_iam_role.lambda_exec.arn
+}
+
+resource "aws_cloudwatch_log_group" "validate_incoming_msg_header" {
+  name = "/aws/lambda/${aws_lambda_function.validate_incoming_msg_header.function_name}"
+
+  retention_in_days = 30
+}
